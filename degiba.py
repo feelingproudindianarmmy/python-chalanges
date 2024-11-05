d22b4102cee8d69511f7ef0927961dc4ce4bef59
feelingproudindianarmy <celestialh4@gmail.com>
2024-11-05 17:54:08 +0545

    python chalanges

---
+++ degiba.py
@@ -0,0 +1,6 @@
+a=float(input("enter any number(numerator):"))
+b=float(input("enter any number(denominator):"))
+if a%b==0:
+    print("Your number is divisivle")
+else:
+    print("Your numbber is not divisivle")
---
+++ python chalange.py
@@ -0,0 +1,14 @@
+#python1
+v=1
+w=4
+z=6
+f=20
+e=v+w**(z//f)
+print(e)
+#python2
+name="rocky"
+age=9
+if name=="rocky" and age>=2:
+    print("You can join our team")
+else:
+    print("You cannot join our team")
